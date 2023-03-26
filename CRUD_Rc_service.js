e1e695fc662719aba9a8a682d353d3c32d544ff3
Brandon Quintero <developerbvth@gmail.com>
2023-03-25 23:16:44 -0500

    Datos ingresados en la conexion empleados

--- CRUD_Rc_service.js
+++ CRUD_Rc_service.js
@@ -1,65 +1,61 @@
-const {MongoClient} = require('mongodb');
+const { MongoClient } = require("mongodb");
 
-const faker = require('faker');
+const faker = require("faker");
 
-
-let arrayN =[]
-let arreglo = []
-for (let i = 0; i < 100; i++) {
-    arreglo = {
-        direccion: faker.address.streetAddress(),
-        nombre: faker.name.findName(),
-        apellido: faker.name.lastName(),
-        telefono: parseInt(faker.phone.phoneNumber()),
-        email: faker.internet.email(),
-        contrasena: faker.internet.password()
-    };
-    const lol = async(arreglo) =>{ 
-        
-        arrayN.push(arreglo)
-        
-    }
-    lol(arreglo)
-    
-    
-}
-
-console.log(arrayN)
-async function main(){
-    
-    const uri = "mongodb+srv://cris:1234@cluster0.g6uloko.mongodb.net/?retryWrites=true&w=majority"
-    const client = new MongoClient(uri);
-
-    
-    try{
-        await client.connect();
-
-        // await client.haceAlgo();
-        // await client.db("admin").command({ping:1}); 
-        // console.log("La conexion fue exitosa");
-        await crearPropiedades(client, arrayN) //Donde llamo la funcion
-        // await encontrarPropiedad(client, "Amos Quitzon")      
-
-
-    }finally{
-        await client.close(); 
+const FakerFuntion = () => {
+    //* utilizamos var para que sean variables globales
+    var arrayN = [];
+    var arreglo = [];
+    for (let i = 0; i < 100; i++) {
+        arreglo = {
+            direccion: faker.address.streetAddress(),
+            nombres: faker.name.findName(),
+            apellidos: faker.name.lastName(),
+            telefono: faker.phone.phoneNumber(),
+            email: faker.internet.email(),
+            contrasena: faker.internet.password(),
+        };
+        const lol = async (arreglo) => {
+            arrayN.push(arreglo);
+        };
+        lol(arreglo);
     }
 
-}
+    console.log(arrayN);
+    async function main() {
+        const uri =
+            "mongodb+srv://Brandon:1allahuakbar123@cluster0.nsvkq9w.mongodb.net/?retryWrites=true&w=majority";
+        const client = new MongoClient(uri);
+
+        try {
+            await client.connect();
+
+            //*  Crear Propiedad
+            // await crearPropiedadesConValidacion(client, arrayN); //Donde llamo la funcion
+            //* Insertar con many
+            await InsertarDatosMany(client, arrayN); //Donde llamo la funcion
+            // await encontrarPropiedad(client, "Amos Quitzon")
+        } finally {
+            await client.close();
+        }
+    }
+
+    main().catch(console.dir);
+};
+FakerFuntion()
 
 
-main().catch(console.dir);
-
-//-----------------------------------------Validar--------------------------------------
+//*-----------------------------------------Validar--------------------------------------
 //todo: ---> VALIDAR DATOS
-// async function crearPropiedades(client, arregloPropiedades){
-//     const result = await client.db("RCservice").createCollection("empleados",{
+// async function crearPropiedadesConValidacion(client, arregloPropiedades){
+//     const result = await client.db("Rcservice").createCollection("proveedores_servicio",{
 //         validator:{
-
-//             $jsonSchema: {
-//                 bsonType: "object",
-//                 required: ["direccion", "nombres","apellidos","telefono","email","contrasena"],
-//             // direccion -- nombres -- apellidos -- telefono -- email -- contraseña
+    
+//                 $jsonSchema: {
+//                         bsonType: "object",
+//                         required: ["identificacion", "nombres","apellidos","email","telefono","servicio","contraseña"],
+//                     //* direccion -- nombres -- apellidos -- telefono -- email -- contraseña
+//                     //* identificacion -- nombres -- apellidos -- telefono -- email --Servicio(s) --contraseña
 //             properties: {
 //                 direccion: {
 //                     bsonType: "string",
@@ -72,12 +68,11 @@ main().catch(console.dir);
 //                 apellidos:{
 //                     bsonType: "string",
 //                     description: "debe ser una cadena y es obligatorio"
-                    
+
 //                 },
 //                 telefono:{
-//                     bsonType: "int",
-//                     pattern: "^\\+?[0-9]{2,20}$",
-//                     description: "debe ser numerico, tipo entero o booleano"
+//                     bsonType: "string",
+//                     description: "debe ser numerico, tipo entero"
 //                 },
 //                 email:{
 //                     bsonType: "string",
@@ -96,34 +91,33 @@ main().catch(console.dir);
 //     console.log(result.insertedIds)
 // }
 
-// Propiedades que va a tener el empleado
-// Propiedades que va a tener el Proveedor
-// identificacion -- nombres -- apellidos -- telefono -- email --Servicio(s) --contraseña
-
-
-// //Insertar 100 documentos en la coleccion de servicios
-async function crearPropiedades(client, arregloPropiedades){
-    const result = await client.db("RCservice").collection("empleados").insertMany(arregloPropiedades);
-    console.log(`Se crearon ${result.insertedCount} nuevas propiedades con los siguientes id(s):`)
-    console.log(result.insertedIds)
+// ----------------------------------------------------------------
+async function InsertarDatosMany(client, arregloPropiedades) {
+    // * InsertMany
+    const result = await client
+        .db("Rcservice")
+        .collection("empleados")
+        .insertMany(arregloPropiedades);
+    console.log(
+        `Se crearon ${result.insertedCount} nuevas propiedades con los siguientes id(s):`
+    );
+    console.log(result.insertedIds);
 }
-// //Propiedades que va a tener los servicios 
-// //id -- NombreSer -- descripcion -- estado -- trabajadore 
+// *id -- NombreSer -- descripcion -- estado -- trabajadore
 
-                                                                                          
-// //-----------------------------------------------------------------------
-// //!Buscar Find()
-// //Buscar un Empleado por su numero nombre 
-async function encontrarPropiedad(client, nombreEmp){
-    const empleado = await client.db("RCservice").collection("Empleados").findOne
-    ({nombre:nombreEmp})
-    if(empleado){
-        console.log(empleado)
-    } else{
-        console.log("No se pudo encontrar al empleado")
+// * -------------------------------------------------------------------------------
+// *Buscar Find()
+async function encontrarPropiedad(client, nombreEmp) {
+    const empleado = await client
+        .db("RCservice")
+        .collection("Empleados")
+        .findOne({ nombre: nombreEmp });
+    if (empleado) {
+        console.log(empleado);
+    } else {
+        console.log("No se pudo encontrar al empleado");
     }
 }
-
 
 // //Buscar un Proveedor de servicio por su numero de Identificacion
 // async function encontrarPropiedad(client, numeroIdentificacion){
@@ -135,7 +129,6 @@ async function encontrarPropiedad(client, nombreEmp){
 //         console.log("No se pudo encontrar al proveedor")
 //     }
 // }
-
 
 // //Buscar un servicio por su nombre
 // async function encontrarPropiedad(client, nombre){
@@ -149,17 +142,16 @@ async function encontrarPropiedad(client, nombreEmp){
 // }
 
 // //----------------------------------------------------------------------------------------
-// //!UpdateMany 
-// //! No me acuerdo como hacer el update 
+// //!UpdateMany
+// //! No me acuerdo como hacer el update
 // async function actualizarPropiedades(client){
 //     const result = await client.db("RCservice").collection("Servicios").updateMany({property_type:{$exists: false}},{$set: {property_type: "Sin definir"}});
-//     console.log(`se actualizaron ${result.matchedCount} propiedades`);     
+//     console.log(`se actualizaron ${result.matchedCount} propiedades`);
 
 // }
 
-
 // //-----------------------------------------------------------------------
-// //Eliminar 1 dato por su nombre 
+// //Eliminar 1 dato por su nombre
 // //!ESTA MELO
 // // async function eliminar1dato(client, nombrePropiedad){
 // //     const dato = await client.db("sample_airbnb").collection("listingsAndRewiews").deleteOne
@@ -170,7 +162,6 @@ async function encontrarPropiedad(client, nombreEmp){
 // //         console.log("La propiedad que desea eliminar no existe")
 // //     }
 // // }
-
 
 // //----------------------------------------------------------------------------------------------------------
 // //Buscar Muchas Propiedades FIND
@@ -191,9 +182,3 @@ async function encontrarPropiedad(client, nombreEmp){
 // //         console.log("Mensaje de error")
 // //     }
 // // }
-
-
-
-
-
-
